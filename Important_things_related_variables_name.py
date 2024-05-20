957dd5767a61b9bde3355fd6e1ce9ae05ba7cead
Malik Nouman <150277895+maliknouman6262@users.noreply.github.com>
2024-05-20 14:04:43 -0700

    Add files via upload

---
+++ Important_things_related_variables_name.py
@@ -0,0 +1,17 @@
+a=45+4
+c=5.7
+b="malik nouman "
+print(b)
+print(a)
+print(c)
+print(a+c)
+
+# rules:
+# 1. variable name should not start with number
+# 2. variable name should not contain any special symbol
+# 3. variable name should not be a keyword
+# 4. variable name should be unique
+# 5. variable name should be descriptive
+# 6. variable name should be in lower case
+# 7. variable name should not be too long
+# 8. variable name should not be too short
